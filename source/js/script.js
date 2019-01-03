eafc07bde2fac71f637ee3e61d0d27cc66a30019
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-01-03 18:57:07 +0300

    Стилизовал навигацию с шапкой, скрываю/показываю выпадающее меню с помощью javascript

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var navButton = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+var navMainWrapper = document.querySelector('.main-nav__wrapper');
+
+navMain.classList.remove('main-nav--nojs');
+
+navButton.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
