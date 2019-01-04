372e5b859463de5f3d58627cd4aff04b2525386d
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-01-04 14:31:51 +0300

    Вставил дату и время на сайт

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//  выпадающее меню
 var navButton = document.querySelector('.main-nav__toggle');
 var navMain = document.querySelector('.main-nav');
 var navMainWrapper = document.querySelector('.main-nav__wrapper');
@@ -16,6 +17,24 @@ navButton.addEventListener('click', function() {
   }
 });
 
-
-
-      
+//  дата и время на сайте
+var startTime = function () {
+  var d = new Date();
+  var day = new Array("Воскресенье","Понедельник","Вторник",
+  "Среда","Четверг","Пятница","Суббота");
+  var month = new Array("января","февраля","марта","апреля","мая","июня",
+  "июля","августа","сентября","октября","ноября","декабря");
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML = 'Сегодня: ' + day[d.getDay()] + ' ' + d.getDate() + ' ' + month[d.getMonth()] + ' ' + h + ":" + m + ":" + s;
+  t = setTimeout('startTime()', 500);
+}
+var checkTime = function (i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
